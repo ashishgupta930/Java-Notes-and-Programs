790907ea387161d3c986ca0638b2856ae0df59ce
ashishgupta930 <ashish.gupta930@gmail.com>
2018-05-03 09:19:43 +0530

    Loops

---
+++ Tutorial 5 For Loops/src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++)
+		{
+			//System.out.println("The Value of i is : " +i);
+			
+			System.out.printf("The Value of i is : %d\n", i);
+		}
+	}
+
+}
