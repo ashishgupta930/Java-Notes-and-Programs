c68f4b730cc773e84daf545ddc3754b8856a6bdd
ashishgupta930 <ashish.gupta930@gmail.com>
2018-05-03 09:23:22 +0530

    If

---
+++ Tutorial 6 If/src/Application.java
@@ -0,0 +1,41 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		boolean cond = 4==5;
+		boolean cond1 = 4==4;
+		boolean cond2 = 4!=5;
+		
+		
+		
+				System.out.println(cond);
+				System.out.println(cond1);
+				System.out.println(cond2);
+				
+				
+				
+				if(4==4 )
+				{
+					System.out.println("Yes its true !");
+				}
+				if(4!=5)
+				{
+					System.out.println("yes its true !");
+				}
+				
+				int myInt = 15;
+				if(myInt<10) {
+					System.out.println("Yes its True !");
+				}
+				else if(myInt>20) {
+				System.out.println("No its false !");
+				}
+				else {
+					System.out.println("None of the above");
+				}
+				
+				
+				
+	}
+
+}
